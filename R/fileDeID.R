83632ca6c36b6b71beb436242d84a1b752e96bad
James Joyce <james@pop-os.localdomain>
2026-02-23 18:28:04 -0500

    R/fileDeID.R: epoch time support added

--- R/fileDeID.R
+++ R/fileDeID.R
@@ -5,7 +5,7 @@
 #' De-identification options include replacing a patient MRN by a tokenized MRN
 #' (using the crosswalk), removing a column, 'blanking' a column
 #' (the column remains in the dataset but all values are ""), and shifting a
-#' date or datetime variable (using the crosswalk).
+#' date, datetime, or Unix epoch time variable (using the crosswalk).
 #'
 #'
 #'
@@ -17,6 +17,10 @@
 #' @param dateformat character. Format of date variables in the extraction file.  Default is "%Y-%m-%d" corresponding to 4-digit year, hyphen, 2-digit month, hyphen, 2-digit day.
 #' @param datetimevariablestodateshift character vector. Names of variables that are datetimes and should be date-shifted. Default is \code{character(0)}, no variables to shift.
 #' @param datetimeformat character. Format of datetime variables in the extraction file.  Default is "%Y-%m-%d %H:%M:%OS" corresponding to 4-digit year, hyphen, 2-digit month, hyphen, 2-digit day, space, 2-digit (24) hour, colon, 2-digit minute, colon 2-digit second.
+#' @param epochvariablestodateshift character vector. Names of variables that represent
+#' Unix epoch time (seconds since 1970-01-01 00:00:00 UTC) and should be shifted.
+#' Values may be stored as integer seconds or floating-point seconds (allowing
+#' fractional seconds). Default is \code{character(0)}, no variables to shift.
 #' @param separator character. Field separator in filetodeid (input file). Default is \code{","}.
 #' @param separator_out character. Field separator to use in outputfile. Default is \code{","}.
 #' @param xwalk data.frame containing crosswalk information. Usually the output from [loadxwalks()].
@@ -24,6 +28,15 @@
 #' @param outputfile character. Name of file to write de-identified data. If \code{NULL} (the default) the data are not written, but only returned from the function. An additional option is the special value \code{"SOURCE_"}, which causes the output to be written to the same filename as the input but prepended with "SOURCE_".
 #' @param usefread If \code{TRUE} (default), use \code{data.table::fread()} and \code{data.table::fwrite()}. If \code{FALSE}, use \code{utils::read.csv()} and \code{utils::write.csv()}. \code{TRUE} is usually preferable as \code{FALSE} results in double quotes around almost all values when producing the output file.
 #' @param verbose integer. Higher values produce more output to console.  Default is 0, no output.
+#'
+#' @details
+#' Date variables are shifted by adding \code{SHIFT_NUM} days.
+#' Datetime variables are shifted by adding \code{86400 * SHIFT_NUM} seconds.
+#' Epoch variables are shifted by adding \code{86400 * SHIFT_NUM} seconds directly
+#' to the numeric epoch value.
+#'
+#' All epoch values are assumed to represent seconds since
+#' 1970-01-01 00:00:00 UTC. No timezone conversion is performed.
 #'
 #' @return (invisibly) data.frame (even if \code{usefread == TRUE}) with variables tokenized, date-shifted, removed, and/or blanked, as requested.
 #' @export
@@ -40,11 +53,12 @@
 #'   variablestoremove = c("Last Name:", "First Name:", "D.o.Birth:"),
 #'   variablestoblank = "Exam Comment:",
 #'   datevariablestodateshift = "Exam Date:",
+#'   epochvariablestodateshift = "ExamTimeUnix",
 #'   dateformat = "%m/%d/%Y",
 #'   xwalk = xwalk,
 #'   outputfile = NULL,
 #'   verbose = 2)
-#' deidfile # Note last test was on a person not in the crosswalk
+#' deidfile
 fileDeID <- function(
     filetodeid,
     fd_varname_mrn = "PAT_MRN",
@@ -54,6 +68,10 @@ fileDeID <- function(
     dateformat = "%Y-%m-%d",
     datetimevariablestodateshift = character(0),
     datetimeformat = "%Y-%m-%d %H:%M:%OS",
+
+    # NEW: epoch variables (seconds since 1970-01-01 UTC)
+    epochvariablestodateshift = character(0),
+
     separator = ",",
     separator_out = ",",
 
@@ -63,7 +81,6 @@ fileDeID <- function(
 
     outputfile = NULL,
     usefread = TRUE,
-
     verbose = 0L) {
 
   if (missing(xwalk))
@@ -77,11 +94,10 @@ fileDeID <- function(
 
   if (!("SHIFT_NUM" %in% names(xwalk)) &&
       ((length(datevariablestodateshift) > 0) ||
-       length(datetimevariablestodateshift) > 0))
-    stop("SHIFT_NUM must be a column in xwalk if any date or datetime variables are to be shifted.")
+       (length(datetimevariablestodateshift) > 0) ||
+       (length(epochvariablestodateshift) > 0)))
+    stop("SHIFT_NUM must be a column in xwalk if any time variables are to be shifted.")
 
-
-  # File to Deidentify
   if (missing(filetodeid))
     stop("Must provide file to deidentify.")
 
@@ -96,25 +112,25 @@ fileDeID <- function(
       header = TRUE,
       na.strings = "",
       sep = separator,
-      check.names = FALSE, # default, actually
+      check.names = FALSE,
       data.table = FALSE,
-      nrows = 0)) # To get variable names
-  } else if (isFALSE(usefread)) {
+      nrows = 0))
+  } else {
     names(utils::read.table(
       filetodeid,
       header = TRUE,
       na.strings = "",
       sep = separator,
-      check.names = FALSE, # allow non-standard names
-      nrows = 0)) # To get variable names
-  } else stop("usefread must be TRUE or FALSE.")
+      check.names = FALSE,
+      nrows = 0))
+  }
 
   if (verbose > 1L) {
     cat(sprintf("%s Variable Names\n", filetodeid))
     print(varnames)
   }
 
-  vartype <- rep("character", length(varnames)) # read all data as character by default
+  vartype <- rep("character", length(varnames))
   names(vartype) <- varnames
 
   if (any(is.na(match(variablestoremove, varnames))))
@@ -125,18 +141,11 @@ fileDeID <- function(
     warning(sprintf("Not all variables in datevariablestodateshift found in %s", filetodeid))
   if (any(is.na(match(datetimevariablestodateshift, varnames))))
     warning(sprintf("Not all variables in datetimevariablestodateshift found in %s", filetodeid))
+  if (any(is.na(match(epochvariablestodateshift, varnames))))
+    warning(sprintf("Not all variables in epochvariablestodateshift found in %s", filetodeid))
   if (is.na(match(fd_varname_mrn, varnames)))
     stop(sprintf("%s not a variable in %s", fd_varname_mrn, filetodeid))
 
-  # this doesn't do anything any more.
-  vartype[fd_varname_mrn] <- if (isTRUE(compare_mrn_numeric)) {
-    # "numeric" # errors if column is quoted in input file
-    "character" # change to numeric after reading
-  } else if (isFALSE(compare_mrn_numeric)) {
-    "character"
-  } else stop("compare_mrn_numeric must be TRUE or FALSE")
-
-  # don't read those variables to be removed
   vartype[intersect(variablestoremove, varnames)] <- "NULL"
 
   dat <- if (isTRUE(usefread)) {
@@ -145,7 +154,7 @@ fileDeID <- function(
       header = TRUE,
       na.strings = "",
       sep = separator,
-      check.names = FALSE, # allow non-standard names
+      check.names = FALSE,
       data.table = FALSE,
       colClasses = vartype)
   } else {
@@ -154,108 +163,99 @@ fileDeID <- function(
       header = TRUE,
       na.strings = "",
       sep = separator,
-      check.names = FALSE, # allow non-standard names
+      check.names = FALSE,
       colClasses = vartype)
   }
 
-  if (verbose > 0L) {
-    cat(sprintf("%d rows read from %s\n", nrow(dat), filetodeid))
-    if (verbose > 1L) {
-      print(utils::head(dat))
-    }
-  }
-
-  # convert character to numeric
   if (isTRUE(compare_mrn_numeric)) {
     dat[, fd_varname_mrn] <- as.numeric(dat[, fd_varname_mrn])
   }
 
-  # blank those to be blanked
-  # dat[intersect(variablestoblank, names(dat))] <- ""
-  # this version is written more compactly by fwrite. i.e., ,, vs ,"",
   dat[, intersect(variablestoblank, names(dat))] <- NA_character_
 
-  # use crosswalk
-  # replace mrn with tokenized mrn
-  # add date_shift variable.
-  # preserve order of dat
   codingindex <- match(dat[, fd_varname_mrn], xwalk[, "PAT_MRN"])
-  if (verbose > 1L) {
-    cat("Summary of MRN matching index\n")
-    print(summary(codingindex))
-  }
-
-  if (verbose > 1L) {
-    cat("Number of patients with each number of test (original MRN)\n")
-    print(table(table(dat[, fd_varname_mrn], useNA = "ifany")))
-  }
-
-  # replace PAT_MRN by PAT_MRN_T
   dat[, fd_varname_mrn] <- xwalk[codingindex, "PAT_MRN_T"]
-  if (verbose > 1L) {
-    cat("Number of patients with each number of test (tokenized MRN)\n")
-    print(table(table(dat[, fd_varname_mrn], useNA = "ifany")))
-  }
 
   if (verbose > 0L)
     cat(sprintf(
       "%6d tokenized mrns missing of %6d\n",
       sum(is.na(dat[, fd_varname_mrn])), nrow(dat)))
 
-  # if there are any variables to date shift, then merge SHIFT_NUM and add to dates
-  if ((length(datevariablestodateshift) > 0L) ||
-      (length(datetimevariablestodateshift) > 0L)) {
-    dat[, "SHIFT_NUM_tEmP"] <- xwalk[codingindex, "SHIFT_NUM"] # add SHIFT_NUM variable
+  # ---------------------------
+  # TIME SHIFTING SECTION
+  # ---------------------------
 
-    # for each date variable, ADD SHIFT_NUM days
+  if ((length(datevariablestodateshift) > 0L) ||
+      (length(datetimevariablestodateshift) > 0L) ||
+      (length(epochvariablestodateshift) > 0L)) {
+
+    dat[, "SHIFT_NUM_tEmP"] <- xwalk[codingindex, "SHIFT_NUM"]
+
+    # Date variables
     for (variabletodateshift in datevariablestodateshift) {
       if (verbose > 0L) cat(sprintf("Shifting %s\n", variabletodateshift))
-      if (verbose > 1L) print(utils::head(dat[, variabletodateshift]))
       dat[, variabletodateshift] <-
         format(
-          as.Date(
-            dat[, variabletodateshift],
-            format = dateformat) +
+          as.Date(dat[, variabletodateshift], format = dateformat) +
             dat[, "SHIFT_NUM_tEmP"],
-          format = "%Y-%m-%d") # output in the SOURCE Date Format Standard
-      if (verbose > 1L) print(utils::head(dat[, variabletodateshift]))
-    }
-    # for each datetime variable, ADD 86400 SHIFT_NUM seconds
-    for (variabletodateshift in datetimevariablestodateshift) {
-      if (verbose > 0L) cat(sprintf("Shifting %s\n", variabletodateshift))
-      if (verbose > 1L) print(utils::head(dat[, variabletodateshift]))
-      dat[, variabletodateshift] <-
-        format(
-          as.POSIXct(
-            dat[, variabletodateshift],
-            format = datetimeformat,
-            tz = "GMT") + # do computation in GMT so Daylight Saving Time not used
-            86400 * dat[, "SHIFT_NUM_tEmP"], # 24 * 60 * 60 = 86400
-          format = "%Y-%m-%d %H:%M:%S", # output in the SOURCE DateTime Format Standard
-          tz = "GMT", # do computation in GMT so Daylight Saving Time not used
-          usetz = FALSE) # TZ not printed
-      if (verbose > 1L) print(utils::head(dat[, variabletodateshift]))
+          format = "%Y-%m-%d")
     }
 
-    # remove SHIFT_NUM from dataset
+    # Datetime variables
+    for (variabletodateshift in datetimevariablestodateshift) {
+      if (verbose > 0L) cat(sprintf("Shifting %s\n", variabletodateshift))
+
+      # fix the problem of leading zeros (if the date string is imported as an integer):
+      pad_left_zero <- function(x, width) {
+		  x <- trimws(format(x, scientific = FALSE))
+		  paste0(strrep("0", pmax(0, width - nchar(x))), x)
+	  }
+      datestrings <- switch(datetimeformat,
+		  "%y%m%d%H%M%S" = pad_left_zero(dat[[variabletodateshift]], 12),
+		  "%y%m%d%H%M"   = pad_left_zero(dat[[variabletodateshift]], 10),
+		  dat[[variabletodateshift]])
+
+      dat[, variabletodateshift] <-
+        format(
+           as.POSIXct(datestrings, format = datetimeformat, tz = "GMT") +
+            86400 * dat[, "SHIFT_NUM_tEmP"],
+          format = "%Y-%m-%d %H:%M:%S",
+          tz = "GMT",
+          usetz = FALSE)
+    }
+
+    # Epoch variables (seconds since 1970-01-01 UTC, integer or float)
+    for (variabletodateshift in epochvariablestodateshift) {
+      if (verbose > 0L) cat(sprintf("Shifting %s (epoch seconds)\n", variabletodateshift))
+      dat[, variabletodateshift] <-
+        as.numeric(dat[, variabletodateshift]) +
+        86400 * dat[, "SHIFT_NUM_tEmP"]
+    }
+
     dat[, "SHIFT_NUM_tEmP"] <- NULL
   }
 
+  # ---------------------------
+  # WRITE OUTPUT
+  # ---------------------------
+
   if (!is.null(outputfile)) {
+
     if (isTRUE(outputfile == "SOURCE_")) {
-      # special case: add SOURCE_ to input filename to create output filename
       sss <- strsplit(filetodeid, split = "/", fixed = TRUE)[[1]]
       sss[length(sss)] <- sprintf("SOURCE_%s", sss[length(sss)])
       outputfile <- paste(sss, collapse = "/")
     }
+
     if (verbose > 0L) cat(sprintf("Writing to %s\n", outputfile))
+
     if (isTRUE(usefread)) {
       data.table::fwrite(
         dat,
         file = outputfile,
         sep = separator_out,
-        row.names = FALSE, # default, actually
-        na = "") # default, actually
+        row.names = FALSE,
+        na = "")
     } else {
       utils::write.table(
         dat,
